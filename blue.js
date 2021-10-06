b66721714dbfdb79cad648f22d267bc63c9f4e31
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-10-06 13:47:31 -0400

    CAT

--- blue.js
+++
@@ -1,6 +0,0 @@
-let button = document.createElement('button')
-button.textContent = 'Click me!'
-document.body.append(button)
-button.addEventListener('click', function() {
-console.log('I was clicked!')
-})
--- questions.js
+++
@@ -1,7 +0,0 @@
-let questions = [
-    {
-      text: `What is your favorite dog? (Out of all 4)?
-        A. Bordoodle
-        B. Poodle
-        C. Lab Retriever
-        D. Pomeranian
